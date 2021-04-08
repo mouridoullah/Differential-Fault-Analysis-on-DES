704b78dbe469d61e4ba94a2d70332bbd0691a0bc
mouridoullah <bdame711@gmail.com>
2021-04-09 00:11:53 +0200

    ajout de commentaire

--- DFAonDES.c
+++ DFAonDES.c
@@ -15,17 +15,20 @@ static long getR(long x) {
 	return a;
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
+// Permuts bits of to permute, according to the table
 static long permutation(long aPermuter, int tableDePermutation[] ,int inputSize, int outputSize) {
 	long res = 0x0L;
 	long bitPermuted, mask;
 	for (int i = 0; i < outputSize; i++) {
+		// The only bit that should be one is the one that will get permuted at this round
 		mask = 0x1L << (inputSize - tableDePermutation[i]);
 		bitPermuted = aPermuter & mask;
 		if (bitPermuted != 0){
 			bitPermuted = 0x1L;
+			// then we shift that bit aPermuter & mask)
 			bitPermuted = bitPermuted << (outputSize - i - 1);
 		} 
-		res = res | bitPermuted;
+		res = res | bitPermuted; // remember that 0 | 1 = 1, 001 | 100 = 101, etc
 	}
 	return res;
 }
@@ -34,6 +37,7 @@ static long expension(long aEtendre) {
 	return permutation(aEtendre, E, 32, 48);
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
+// left shift a 32 bits integer circularly
 static long rotationCirculaire(long x) {
 	long poidsFort;
 	long n = (long)pow(2, 27);
@@ -47,6 +51,7 @@ static long rotationCirculaire(long x) {
     return x;
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
+// left shift the two halves of a 32 bits integer circularly
 static long leftShift(long T) {
     long C = (T & 0xFFFFFFF0000000L) >> 28;
     long D = T & 0x0000000FFFFFFFL;
@@ -75,11 +80,13 @@ static long* keySchedule(long cleffMaitresse) {
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 static long f_function(long R, long subKeys) {
+	// after expansion, R is 48 bits long
 	long block, msbDoubled, lsb;
 	int line, column;
 	long T = expension(R) ^ subKeys;
 	long res = 0x0L;
 	for (int i = 1 ; i < 9 ; i++){
+		// 48 bits / 8 = 8 block of 6 bits to be handle separately 
 		block = (T >> (48 - i*6)); 
 		
 		msbDoubled = (block & 0b100000) >> 4;
@@ -87,20 +94,25 @@ static long f_function(long R, long subKeys) {
 	
 		line = (int)(msbDoubled + lsb);
 		column = (int) ((block & 0b011110) >> 1);
-		
+
+		// Then every block goes throught one SBox => 4 bits outputed
+        // for each Sbox, append it's output to the result
 		res = res | (Sbox[i - 1][line][column] << (32 - i*4));
 	}
 	return permutation(res, P, 32, 32);
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 static long DES(long clair, long K) {
+	// Derivating K into 16 subkeys
 	long* subKeys =  keySchedule(K);
 	
+	// Inital Permutation, IP
 	long clairIP = permutation(clair, IP, 64, 64);
 	
 	long L = getL(clairIP);
 	long R = getR(clairIP);
 	
+	// Feistel round/subkey
 	long LiPlus1, RiPlus1;
 	for (int i = 0; i < 16; i++) {
 		LiPlus1 = R;
@@ -108,7 +120,10 @@ static long DES(long clair, long K) {
 		L = LiPlus1;
 		R = RiPlus1;	
 	}
+	// swap R16 and L16
 	long swapped = (R << 32) | L;
+
+	// apply IP^-1
 	return permutation(swapped, IPinv, 64, 64);
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
@@ -130,6 +145,7 @@ static long rechercheDeK16ParmisToutesLesPossibites(long K16, int sol[][6][64], 
 			candidat = (long) sol[s][0][numSolf0];
 		}
 	}
+	// the solution is the only block that is common to every set of solutions for the current sbox
 	printf("Solution %d = ",(s+1));
 	K16 = K16 << 6;
 	K16 = K16 | candidat;
@@ -138,22 +154,22 @@ static long rechercheDeK16ParmisToutesLesPossibites(long K16, int sol[][6][64], 
 	return K16;
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
-static void isSolutionValide(long verif, long E_R15, long E_R15f, int sol[][6][64], int nbSol[][6], int s, int f) {
+static void isSolutionValide(long P_1_L16_xor_L16f, long E_R15, long E_R15f, int sol[][6][64], int nbSol[][6], int s, int f) {
 	long tmp; long tmpf; int r; int c; int rf; int cf;
 	for (int k16i = 0; k16i < 64; k16i++) {
 		tmp = ((E_R15 & mask6[s]) >> ((7 - s) * 6)) ^ k16i;
 		tmpf = ((E_R15f & mask6[s]) >> ((7 - s) * 6)) ^ k16i;
-		/*-------------------------------------------*/
+
 		r = (int) (2 * ((tmp & 0x20) >> 5) + (tmp & 0x1));
 		c = (int) ((tmp & 0x1E) >> 1);
-		/*-------------------------------------------*/
+
 		rf = (int) (2 * ((tmpf & 0x20) >> 5) + (tmpf & 0x1));
 		cf = (int) ((tmpf & 0x1E) >> 1);
-		/*-------------------------------------------*/
-		int PP = (int) ((verif & mask4[s]) >> ((7 - s) * 4));
-		int SS = Sbox[s][r][c] ^ Sbox[s][rf][cf];
-		/*-------------------------------------------*/
-		if ( PP ==	SS ) {	
+
+		int ver = (int) ((P_1_L16_xor_L16f & mask4[s]) >> ((7 - s) * 4));
+		int xor = Sbox[s][r][c] ^ Sbox[s][rf][cf];
+
+		if ( ver ==	xor ) {	
 			sol[s][f][nbSol[s][f]] = k16i;
 			++nbSol[s][f];
 		}
@@ -173,28 +189,30 @@ static void afficherLesPotentiellesCles(int sol[][6][64], int nbSol[][6], int s)
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 long find_K16(long chiffreJuste, long chiffreFaux[]) {
 	long K16 = 0x000000000000L;
-	long dechiffJuste, dechiffFaux, L16, R15, L16f, R15f, verif, E_R15, E_R15f;
-	/*-------------------------------------------*/
+	long dechiffJuste, dechiffFaux, L16, R15, L16f, R15f, P_1_L16_xor_L16f, E_R15, E_R15f;
+
+
 	int sol[8][6][64];
 	int nbSol[8][6];
 	
-	/*-------------------------------------------*/
+
 	dechiffJuste = permutation(chiffreJuste, IP, 64, 64); 
 	L16 = getL(dechiffJuste); 
 	R15 = getR(dechiffJuste);
-	/*-------------------------------------------*/
+
+	// Attacking the 8 sboxes with the equation we found
 	for (int s = 0; s < 8; s++) {
 		for (int f = 0; f < 6; f++) {
 			dechiffFaux = permutation(chiffreFaux[faux[s][f]], IP, 64, 64);
 			L16f = getL(dechiffFaux); 
 			R15f = getR(dechiffFaux);
-			/*-------------------------------------------*/
 
-			verif = permutation(L16 ^ L16f, Pinv, 32, 32);
+
+			P_1_L16_xor_L16f = permutation(L16 ^ L16f, Pinv, 32, 32);
 			E_R15 = permutation(R15, E, 32, 48); 
 			E_R15f = permutation(R15f, E, 32, 48);
-			/*-------------------------------------------*/
-			isSolutionValide(verif, E_R15, E_R15f, sol, nbSol, s, f);
+			
+			isSolutionValide(P_1_L16_xor_L16f, E_R15, E_R15f, sol, nbSol, s, f);
 		}
 		afficherLesPotentiellesCles(sol, nbSol, s);
 		K16 = rechercheDeK16ParmisToutesLesPossibites(K16, sol, nbSol, s);
@@ -202,24 +220,29 @@ long find_K16(long chiffreJuste, long chiffreFaux[]) {
 	return K16;
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
-static long rechercheDes8bitsInconnus(long mask) {
-	long pos[] = {14, 15, 19, 20, 51, 54, 58, 60};
+static long rechercheDes8bitsPerdus(long mask) {
+	long positionsToRecover[] = {14, 15, 19, 20, 51, 54, 58, 60};
 	long res = 0x0L;
 	
 	for(int i = 0; i < 8; i++) 
-		res = res | ( ((mask >> i) & 1) << (64 - pos[i]) );
+		res = res | ( ((mask >> i) & 1) << (64 - positionsToRecover[i]) );
 
 	return res;
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 static long find_K56(long clair, long chiffre, long K16) {
+	// We put the 48 bits of K16 at their position in K (going backwards in the key schedule)
+    // Inverse of PC2 : 48 -> 56 bits, 8 wrong bits which's positions are known
+    // Inverse of PC1 : 56 -> 64 bits, 8 wrong bits (parity, no consequence on the calculation). However our 8 wrong 
+    // bits from PC2 are still wrong, but in different positions that are still trackable
 	long K48b = permutation(permutation(K16, PC2inv, 48, 56), PC1inv, 56, 64);
+	
 	long mask = 0x00L;
 	long hypothesis = K48b;
 	
 	while( mask < 256 && chiffre != DES(clair, hypothesis) ) 
 	{
-		hypothesis = K48b | rechercheDes8bitsInconnus(mask);
+		hypothesis = K48b | rechercheDes8bitsPerdus(mask);
 		mask = mask + 1;
 	}
 	if (mask == 256)
@@ -229,6 +252,7 @@ static long find_K56(long clair, long chiffre, long K16) {
 }
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 static long find_K(long K56b) {
+	// parity
 	long res = K56b;
 	long tmp, parite;
 	
@@ -248,6 +272,8 @@ static long find_K(long K56b) {
 	}
 	return res;
 }
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 int main(){
 	long clair = 0xDF1549D00BA1E0C4L; 
